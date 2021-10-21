899e8aee4f67e2c60fffb929d826146dc8c5ae99
KodaiTakano <takano_x_kodai@icloud.com>
2021-10-21 10:59:13 +0900

    日付表示

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,7 @@
+import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/post'
+import Date from '../../components/date'
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -16,18 +18,21 @@ export async function getStaticProps({ params }) {
         postData
       }
     }
-  }
+}
 
-  export default function Post({ postData }) {
+export default function Post({ postData }) {
     return (
       <Layout>
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
         {postData.title}
         <br />
         {postData.id}
         <br />
-        {postData.date}
+        <Date dateString={postData.date} />
         <br />
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </Layout>
     )
-  }
+}
